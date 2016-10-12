64ba3e884ddaa139cdc1b33e2e93b6ff5df232a0
Salomon ROSSELL <salomon.rossell@gmail.com>
2016-10-12 23:10:31 +0200

    Add constants Create bot game

--- public/js/Constants.js
+++ public/js/Constants.js
@@ -3,10 +3,17 @@ app.factory('Constants', [function() {
     ///////INFO AT http://codeandplay.date/public/api.html /////////////////////////
 
     return {
+        UNKNOWN: 'NA',
+
         STATUS_YES: 'OUI',
         STATUS_NO: 'NON',
         STATUS_WIN: 'GAGNE',
         STATUS_LOST: 'PERDU',
-        STATUS_CANCEL: 'ANNULE'
+        STATUS_CANCEL: 'ANNULE',
+
+        MOVE_OK: 'OK',
+        MOVE_KO: 'KO',
+        MOVE_WIN: 'GAGNE',
+        MOVE_NOT_YOUR_TURN: 'PTT'
     }
 }]);
--- public/js/MainCtrl.js
+++ public/js/MainCtrl.js
@@ -1,14 +1,16 @@
 // Creates the MainCtrl Module and Controller.
-app.controller('MainCtrl', ['$scope', '$window', '$http', '$log', 'Urls', 'Constants', function($scope, $window, $http, $log, Urls, Constants) {
+app.controller('MainCtrl', ['$scope', '$window', '$http', '$log', '$timeout', 'Urls', 'Constants', function($scope, $window, $http, $log, $timeout, Urls, Constants) {
     $log.log('hello biach');
 
     var pingUrl = Urls.BASE_URL + Urls.PING;
-
+    var createBotGameUrl = Urls.BASE_URL + Urls.INIT_BOT_GAME;
     //only for tests
     var teamPassword = '/iMOBILE';
-
     var teamIdUrl = Urls.BASE_URL + Urls.USER + teamPassword;
+
+    //GAME variables
     var teamId;
+    var botGameId;
 
     /**
      * test ping -> response = pong
@@ -22,16 +24,50 @@ app.controller('MainCtrl', ['$scope', '$window', '$http', '$log', 'Urls', 'Const
     }
 
     /**
+     * generic http call
+     */
+    function sendRequest (url) {
+        return $http.get(url);
+    }
+
+    /**
+     * generic error handling
+     */
+    function logError (error) {
+        $log.error(error);
+    }
+
+    /**
      * retrieve teamId
      */
     $scope.getTeamId = function () {
         if (!teamId) {
-            $http.get(teamIdUrl).then(function(response) {
+           sendRequest(teamIdUrl).then(function(response) {
                 $log.log(response.data);
                 teamId = response.data;
-            }, function(error) {
-                $log.log(error);
-            });
+            }, logError);
+        }
+    };
+
+    /**
+     * create new bot game
+     */
+    var level = 1;
+    $scope.createGame = function() {
+        if (teamId) {
+            createBotGameUrl = createBotGameUrl.replace('@level', level).replace('@teamId', teamId);
+            sendRequest(createBotGameUrl).then(function(response) {
+                var data = response.data;
+                $log.log(data);
+
+                if (data !== Constants.UNKNOWN) {
+                    //game created
+                    botGameId = data;
+                } else {
+                    //game not created yet
+                    $timeout($scope.createGame(), 200);
+                }
+            }, logError);
         }
     };
 
