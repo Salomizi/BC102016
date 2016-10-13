4de8e509071313cb2b9dc50ff339ed5d45034ce2
Salomon ROSSELL <salomon.rossell@gmail.com>
2016-10-13 21:33:43 +0200

    Premieres implémentations

--- public/js/MainCtrl.js
+++ public/js/MainCtrl.js
@@ -1,34 +1,55 @@
 // Creates the MainCtrl Module and Controller.
-app.controller('MainCtrl', ['$scope', '$window', '$http', '$log', '$timeout', 'Urls', 'Constants', function($scope, $window, $http, $log, $timeout, Urls, Constants) {
+app.controller('MainCtrl', ['$scope', '$window', '$http', '$q', '$log', '$timeout', 'Urls', 'Constants', function($scope, $window, $http, $q, $log, $timeout, Urls, Constants) {
     $log.log('hello biach');
 
     var pingUrl = Urls.BASE_URL + Urls.PING;
+    var teamIdUrl = Urls.BASE_URL + Urls.USER;
     var createBotGameUrl = Urls.BASE_URL + Urls.INIT_BOT_GAME;
-    //only for tests
-    var teamPassword = '/iMOBILE';
-    var teamIdUrl = Urls.BASE_URL + Urls.USER + teamPassword;
+    var gameStatusUrl = Urls.GAME_STATUS;
+    var gameBoardUrl = Urls.GAME_BOARD;
+    var makeMoveUrl = Urls.GAME_MAKE_MOVE;
 
     //GAME variables
-    var teamId;
-    var botGameId;
+    $scope.teamId = null;
+    $scope.gameId = null;
+    $scope.botLevel = null;
+
+    /**
+     * generic request
+     * $http does not work with the test API
+     * @param url : request url
+     * @return request response
+     */
+    function sendRequest (url) {
+        var deferred = $q.defer();
+        var req = new XMLHttpRequest();
+        req.open('GET', url, true); 
+        req.onreadystatechange = function () {
+            if (req.readyState == 4) {
+                deferred.resolve(req.response);
+            }
+        };
+        req.onerror = function onError (error) {
+            $log.log('[REQUEST] error : ' + error);
+            deferred.reject();
+        };
+
+        $timeout(function() {
+            req.send(null);       
+        }, 200);
+
+        return deferred.promise;
+    }
 
     /**
      * test ping -> response = pong
      */
     function testPing () {
-        $http.get(pingUrl).then(function(response) {
-            $log.log(response.data);
-        }, function(error) {
-            $log.log(error);
+        sendRequest(pingUrl).then(function(response) {
+            $log.log(response);
         });
     }
-
-    /**
-     * generic http call
-     */
-    function sendRequest (url) {
-        return $http.get(url);
-    }
+    testPing();
 
     /**
      * generic error handling
@@ -38,13 +59,73 @@ app.controller('MainCtrl', ['$scope', '$window', '$http', '$log', '$timeout', 'U
     }
 
     /**
+     * analyse game board
+     * @return move to make
+     */
+    function computeNextMove (board) {
+        //TODO intelligence de batard
+        return Constants.MOVE_SHOOT;
+    }
+
+    /**
+     * analyse status
+     */
+    function computeGameStatus (status) {
+        if (status === Constants.STATUS_YES) {
+            return true;
+        } else if (status === Constants.STATUS_NO) {
+            return false;
+        } else {
+            //something went wrong, or we won !
+            $log.log(status);
+            return null;
+        }
+    }
+
+    /**
+     * analyse move response
+     */
+    function continueGame (moveResult) {
+
+    }
+
+
+    /**
+     * play game
+     */
+    var gameOver = false;
+    function play () {
+        //update urls
+        gameBoardUrl = gameBoardUrl.replace('@idPartie', $scope.gameId);
+        gameStatusUrl = gameStatusUrl.replace('@idPartie', $scope.gameId);
+
+        if (!gameOver) {
+            sendRequest(gameBoardUrl).then(function(board) {
+                var nextMove = computeNextMove(board);
+                sendRequest(gameStatusUrl).then(function(status) {
+                    var canPlay = computeGameStatus(status);
+                    if (status !== null && status) {
+                        makeMoveUrl = makeMoveUrl.replace('@move', nextMove);
+                        sendRequest(makeMoveUrl).then(function(result) {
+                            //var canContinue = 
+                        });
+                    } else {
+                        gameOver = true;
+                    }
+                });
+            });
+        }
+    }
+
+
+    /**
      * retrieve teamId
      */
     $scope.getTeamId = function () {
-        if (!teamId) {
+        if (!$scope.teamId) {
            sendRequest(teamIdUrl).then(function(response) {
-                $log.log(response.data);
-                teamId = response.data;
+                $log.log(response);
+                $scope.teamId = response;
             }, logError);
         }
     };
@@ -54,18 +135,18 @@ app.controller('MainCtrl', ['$scope', '$window', '$http', '$log', '$timeout', 'U
      */
     var level = 1;
     $scope.createGame = function() {
-        if (teamId) {
-            createBotGameUrl = createBotGameUrl.replace('@level', level).replace('@teamId', teamId);
+        if ($scope.teamId && $scope.botLevel) {
+            createBotGameUrl = createBotGameUrl.replace('@level', $scope.botLevel).replace('@teamId', $scope.teamId);
             sendRequest(createBotGameUrl).then(function(response) {
-                var data = response.data;
-                $log.log(data);
+                $log.log(response);
 
-                if (data !== Constants.UNKNOWN) {
+                if (response !== Constants.UNKNOWN) {
                     //game created
-                    botGameId = data;
+                    $scope.gameId = response;
+                    //play();
                 } else {
                     //game not created yet
-                    $timeout($scope.createGame(), 200);
+                    $timeout($scope.createGame(), 300);
                 }
             }, logError);
         }
--- public/js/Urls.js
+++ public/js/Urls.js
@@ -4,21 +4,21 @@ app.factory('Urls', [function() {
 
     return {
         BASE_URL: 'http://www.codeandplay.date',
-        PING: '/battle-ws/ping',
-        USER: '/battle-ws/player/getIdEquipe/iMOBILE/CeciEstUneRevolution!',
+        PING: '/battle-ws/duel/ping',
+        USER: '/battle-ws/duel/player/getIdEquipe/iMOBILE/CeciEstUneRevolution!',
 
         //vs player
-        INIT_PLAYER_GAME: '/battle-ws/versus/next/@teamId',
+        INIT_PLAYER_GAME: '/battle-ws/duel/versus/next/iMOBILE',
 
         //vs bots       
-        INIT_BOT_GAME: '/battle-ws/practice/new/@level/@teamId',
-        ID_BOT_GAME: '/battle-ws/practice/next/@teamId',
+        INIT_BOT_GAME: '/battle-ws/duel/practice/new/@level/iMOBILE',
+        ID_BOT_GAME: '/battle-ws/duel/practice/next/iMOBILE',
 
         //Gameplay
-        GAME_STATUS: '/battle-ws/game/status/@idPartie/@idEquipe',
-        GAME_BOARD: '/battle-ws/game/board/@idPartie',
-        GAME_LAST_MOVE: '/battle-ws/game/getlastmove/@idPartie',
-        GAME_MAKE_MOVE: '/battle-ws/game/play/@idPartie/@idEquipe/@move'
+        GAME_STATUS: '/battle-ws/duel/game/status/@idPartie/iMOBILE',
+        GAME_BOARD: '/battle-ws/duel/game/board/@idPartie',
+        GAME_LAST_MOVE: '/battle-ws/duel/game/getlastmove/@idPartie',
+        GAME_MAKE_MOVE: '/battle-ws/duel/game/play/@idPartie/iMOBILE/@move'
 
 
         //TODO complete urls
